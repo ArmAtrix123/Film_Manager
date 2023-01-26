fc3d472362bcd3a0e197d37a49d3742712a67e97
Merfy <96495867+ArmAtrix123@users.noreply.github.com>
2023-01-26 16:31:20 +0300

    create: interactive

--- js/script.js
+++ js/script.js
@@ -81,13 +81,6 @@
 
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
 Отсортировать их по алфавиту 
 
@@ -105,3 +98,25 @@ const movieDB = {
     ]
 };
 
+const removeAdv = document.querySelectorAll(".promo__adv img"),
+      poster = document.querySelector(".promo__bg"),
+      changeGenre = poster.querySelector(".promo__genre"),
+      changeListMovie = document.querySelector(".promo__interactive-list");
+
+changeGenre.textContent = 'Драма';
+removeAdv.forEach(item => {
+    item.remove();
+});
+
+poster.style.backgroundImage = 'url(../img/bg.jpg)';
+
+changeListMovie.innerHTML = '';
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) =>{
+    changeListMovie.innerHTML += `
+        <li class="promo__interactive-item"> ${i+1}) ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
